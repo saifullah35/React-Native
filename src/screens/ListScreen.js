a029a664870b774c6fa8f95c9a9aae8f4af79e92
Saif <saif@Arifs-MBP-4.lan>
2024-01-25 13:17:28 -0500

    few props around FlatList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,31 +1,37 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList} from 'react-native';
 
-const ListScreen = () => {
-    const friends = [
-        {name: 'Friend #1'},
-        {name: 'Friend #2'},
-        {name: 'Friend #3'},
-        {name: 'Friend #4'},
-        {name: 'Friend #5'},
-        {name: 'Friend #6'},
-        {name: 'Friend #7'},
-        {name: 'Friend #8'},
-        {name: 'Friend #9'},
-    ];
+    const ListScreen = () => {
+        const friends = [
+            {name: 'Friend #1'},
+            {name: 'Friend #2'},
+            {name: 'Friend #3'},
+            {name: 'Friend #4'},
+            {name: 'Friend #5'},
+            {name: 'Friend #6'},
+            {name: 'Friend #7'},
+            {name: 'Friend #8'},
+            {name: 'Friend #9'},
+        ];
 
-    return (
-    <FlatList 
-        keyExtractor={friend => friend.name}
-        data={friends} 
-        renderItem={({item}) => {
-            return <Text>{item.name}</Text>;
-        }}
-        />
-    );
-};
+        return (
+        <FlatList 
+            horizontal   //Tells list to scroll horizontally
+            showsHorizontalScrollIndicator={false} //keeps that scrollbar from appearing
+            keyExtractor={friend => friend.name}  //Specifies how to extraxt the key for each item
+            data={friends}   //Array of friends passed as the data source for the list
+            renderItem={({item}) => { //Function that takes an object with the 'item' prop and returns the JSX to render for each item
+                return <Text style={styles.textStyle}>{item.name}</Text>;
+            }}
+            />
+        );
+    };
 
-const styles = StyleSheet.create({});
+    const styles = StyleSheet.create({
+        textStyle:{
+            marginVertical: 50
+        }
+});
 
 export default ListScreen;
 
