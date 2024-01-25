6783b993bd5b44630fc807b642979d9f2c14dc5a
Saif <saif@Arifs-MBP-4.lan>
2024-01-25 12:26:08 -0500

    JSX exercise and solution

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Text, StyleSheet, View} from 'react-native';
 
 const ComponentsScreen = () => {
-    const greeting = <Text>Hello to you!</Text>;
+    const name = 'Saif Ullah';
 
     return (
         <View>
-            <Text style={styles.textStyle}>This is the components screen</Text>
-            {greeting}
+            <Text style={styles.textStyle}>Getting started with React Native!</Text>
+            <Text style={styles.textStyle2}>My name is {name}</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
+    },
+    textStyle2: {
+        fontSize: 20
     }
 });
 
