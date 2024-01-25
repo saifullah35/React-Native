12a91af66b35a6099a2139e0801db4afeb0ee127
Saif <saif@Arifs-MBP-4.lan>
2024-01-25 14:22:30 -0500

    Destructuring props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {  //Using navigation savestime by not having to insert props
   return (
     <View>
-      <Text style={styles.text}>Hi There</Text>
+      <Text style={styles.text}>Hi There!</Text>
       <Button 
-        onPress={() => props.navigation.navigate('Components')}
         title="Go to Components Demo"
+        onPress={() => navigation.navigate('Components')}
       />
-      <TouchableOpacity onPress={() => console.log('List pressed')}>
-        <Text>Go to List Demo</Text>
-        <Text>Go to List Demo</Text>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate('List')}
+      />
     </View>
   );
 };
