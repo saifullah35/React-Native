d82c917901add11fa42831244fdd97b894a61345
Saif <saif@Arifs-MBP-4.lan>
2024-01-30 12:30:10 -0500

    showing colors with a FlatList

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, FlatList} from 'react-native';
 
 const ColorScreen = () => {
     const [ colors, setColors ] = useState([]);
@@ -7,12 +7,21 @@ const ColorScreen = () => {
     return (
         <View>
             <Button 
-                title="Add a color" 
-                onPress ={ () => 
+                title= "Add a color" 
+                onPress= {() => 
                     setColors([...colors, randomeRgb])
                 }
             />
             <View style={{ height: 100, width: 100, backgroundColor: randomeRgb }} />
+            <FlatList 
+                keyEctractor={(item) => item}
+                data= {colors} //array of data that we want to turn into a list
+                renderitem= {({ item }) => {
+                    return (
+                        <View style={{ height: 100, width: 100, backgroundColor: item }} />
+                    ); //function that that's going to be called with every element inside the array
+                }}  
+            />
         </View>
     );
 };
