ec018bb0059664cd02b861d02303ad89d9f17293
Saif <saif@Arifs-MBP-4.lan>
2024-01-30 12:24:43 -0500

    Adding random colors

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Button } from 'react-native';
 
 const ColorScreen = () => {
+    const [ colors, setColors ] = useState([]);
+
     return (
         <View>
-            <Button title="Add a color" />
+            <Button 
+                title="Add a color" 
+                onPress ={ () => 
+                    setColors([...colors, randomeRgb])
+                }
+            />
             <View style={{ height: 100, width: 100, backgroundColor: randomeRgb }} />
         </View>
     );
