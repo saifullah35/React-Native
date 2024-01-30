96e003fbbc23ac0b535cf405da1a4736930a320b
Saif <saif@Arifs-MBP-4.lan>
2024-01-30 14:17:37 -0500

    excercise solution

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,25 +1,38 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+    // state === {count: number}
+    // action === { type: 'increment' || 'decrement', payload: 1}
+
+    switch (action.type) {
+        case 'increment':
+            return { ...state, count: state.count + action.payload };
+        case 'decrement':
+            return { ...state, count: state.count - action.payload };
+        default:
+            return state;
+    }
+};
+
 const CounterScreen = () => {
-    const [counter, setCounter ] = useState(0);
+    const [state, callMyReducer] = useReducer(reducer, { count: 0 })
 
     return (
         <View>
             <Button 
                 title= "Increase" 
                 onPress={() => {
-                //counter++; // DONT DO THIS
-                    setCounter(counter + 1);
+                    callMyReducer({ type: 'increment', payload: 1})
                 }}
             />
             <Button 
                 title= "Decrease" 
                 onPress={() => {
-                    setCounter(counter - 1);
+                    callMyReducer({ type: 'decrement', payload: 1})
                 }}
             />
-            <Text>Current Count: {counter} </Text>
+            <Text>Current Count: {state.count} </Text>
         </View>
     );
 };
