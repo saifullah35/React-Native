178dd6601f4c8549c8f6f44122e0d70563f9a603
Saif <saif@Arifs-MBP-4.lan>
2024-01-30 14:07:52 -0500

    community convention in reducers

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,18 +5,19 @@ import ColorCounter from "../components/ColorCounter";
 const COLOR_INCREMENT = 15;
 
 const reducer = (state, action) => {
-    // state === {red: number, green: number, blue: number }
-    // action === {colorToChange: 'red' || 'green' || 'blue', amoubt:15 || -15}
-    switch (action.colorToChange) {
-        case 'red':
-            return state.red + action.amount > 255 || state.red + action.amount < 0
-            ? state : {...state, red: state.red + action.amount }
-        case 'green':
-            return state.green + action.amount > 255 || state.green + action.amount < 0
-            ? state : {...state, green: state.green + action.amount }
-        case 'blue':
-            return state.blue + action.amount > 255 || state.blue + action.amount < 0
-            ? state : {...state, blue: state.blue + action.amount }
+    // state === { red: number, green: number, blue: number }
+    // action === { type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15 }
+
+    switch (action.type) {
+        case 'change_red':
+            return state.red + action.payload > 255 || state.red + action.payload < 0
+            ? state : {...state, red: state.red + action.payload }
+        case 'change_green':
+            return state.green + action.payload > 255 || state.green + action.payload < 0
+            ? state : {...state, green: state.green + action.payload }
+        case 'change_blue':
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0
+            ? state : {...state, blue: state.blue + action.payload }
         default:
             return state;
     }
@@ -29,18 +30,18 @@ const SquareScreen = () => {
     return (
         <View>
             <ColorCounter 
-                onIncrease={() => runMyReducer({ colorToChange: 'red', amount: COLOR_INCREMENT })} 
-                onDecrease={() => runMyReducer({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })}
+                onIncrease={() => runMyReducer({ type: 'change_red', payload: COLOR_INCREMENT })} 
+                onDecrease={() => runMyReducer({ type: 'change_red', payload: -1 * COLOR_INCREMENT })}
                 color="Red" 
             />
             <ColorCounter 
-                onIncrease={() => runMyReducer({ colorToChange: 'blue', amount: COLOR_INCREMENT })} 
-                onDecrease={() => runMyReducer({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })}
+                onIncrease={() => runMyReducer({ type: 'change_blue', payload: COLOR_INCREMENT })} 
+                onDecrease={() => runMyReducer({ type: 'change_blue', payload: -1 * COLOR_INCREMENT })}
                 color="Blue"
             />
             <ColorCounter 
-                onIncrease={() => runMyReducer({ colorToChange: 'green', amount: COLOR_INCREMENT })} 
-                onDecrease={() => runMyReducer({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })}
+                onIncrease={() => runMyReducer({ type: 'change_green', payload: COLOR_INCREMENT })} 
+                onDecrease={() => runMyReducer({ type: 'change_green', payload: -1 * COLOR_INCREMENT })}
                 color="Green"
             />
             <View 
