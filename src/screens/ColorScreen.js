63235f6d12ed876647f68c70dd23427f91f163b8
Saif <saif@Arifs-MBP-4.lan>
2024-01-30 12:19:13 -0500

    Generating random colors

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -5,9 +5,18 @@ const ColorScreen = () => {
     return (
         <View>
             <Button title="Add a color" />
+            <View style={{ height: 100, width: 100, backgroundColor: randomeRgb }} />
         </View>
     );
 };
+
+const randomeRgb = () => {
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
 
 const styles = StyleSheet.create({});
 
